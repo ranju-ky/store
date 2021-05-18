7e43a8380c9fbc7054833a60673f497326249007
Ranju- <ranjuprj98@gmail.com>
2021-05-18 15:34:21 +0200

    no puedo seguir a reglar delete metodo

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -1,7 +1,13 @@
 package org.factoriaf5.store.controllers;
 
+import org.factoriaf5.store.model.Product;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 public class ProductController {
@@ -11,5 +17,26 @@ public class ProductController {
         return greeting;
 
     }
+    private List<Product> products = new ArrayList<>();
+    @GetMapping("/prouducts")
+    public List<Product> allproducts(){
+        return products;
+    }
+
+    @PostMapping("/prouducts")
+       public addProduct(Product product){
+        products.add(product);
+
+        @DeleteMapping("/products")
+         public Product findProduct(String,
+                name, List<Product> products),
+         List<product>){
+           for (product.getName().equals(name)) {
+                return product;
+            }
+        }
+
+    }
+
 
 }
